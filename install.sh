728a9503152315cc6456c10a41ac65468f3ef125
Byteingpython <124276113+Byteingpython@users.noreply.github.com>
2024-04-23 16:34:58 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+cd $HOME
+sudo pacman -Syu
+sudo pacman -S git --noconfirm
+
+sudo pacman -S --needed base-devel --noconfirm
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+gh auth login
+
+paru -S github-cli chezmoi zsh zsh-theme-powerlevel10k-git neovim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+chezmoi init https://github.com/Byteingpython/dotfiles.git
+chezmoi apply -v
+
+chsh /bin/zsh
