86d62f26958ab7bb74c61e5e0140c5e5df884a58
Byteingpython <124276113+Byteingpython@users.noreply.github.com>
2024-04-23 16:42:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ makepkg -si --noconfirm
 
 gh auth login
 
-paru -S github-cli chezmoi zsh zsh-theme-powerlevel10k-git neovim
+paru -S github-cli chezmoi zsh zsh-theme-powerlevel10k-git neovim --noconfirm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 chezmoi init https://github.com/Byteingpython/dotfiles.git
