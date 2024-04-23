305a4aa9c41ccd6873dddeb0f05b918eff318c53
Byteingpython <124276113+Byteingpython@users.noreply.github.com>
2024-04-23 16:55:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ cd $HOME
 sudo pacman -Syu --noconfirm
 sudo pacman -S git --noconfirm
 
-sudo pacman -S --needed base-devel --noconfirm
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si --noconfirm
+# sudo pacman -S --needed base-devel --noconfirm
+# git clone https://aur.archlinux.org/paru.git
+# cd paru
+# makepkg -si --noconfirm
 
 paru -S github-cli chezmoi zsh zsh-theme-powerlevel10k-git neovim --noconfirm
 gh auth login
